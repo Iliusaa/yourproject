8b3c17104c81ad425f40ba51ec6699cd6e8e8886
Ilia <iliushadd.md@gmail.com>
2026-09-20 21:17:39 +0300

    clone done, print done

--- Komp.java
+++ Komp.java
@@ -7,6 +7,7 @@ public class Komp{
     float weight[];
     float year[];
     double price[];
+    static int ammountKomp;
     
     Komp(){
         namePerson = "Ilia";
@@ -27,17 +28,67 @@ public class Komp{
         for (int i = 0; i < price.length; i++) {
             price[i]=3;
         }
+        ammountKomp++;
     }
 
-    Komp(byte components, String namePerson) {
-        this.components = components;
+    Komp(String namePerson, byte components) {
         this.namePerson = namePerson;
+        this.components = components;
+        partModel=new String[1];
+        ammountKomp++;
     }
+     Komp(Komp Clone) {
+        namePerson=Clone.namePerson;
+        components = Clone.components;
+        partModel=new String[Clone.partModel.length];
+        for (int i = 0; i < Clone.partModel.length; i++) {
+        partModel[i] = Clone.partModel[i];
+        }
+        weight = new float[Clone.weight.length];
+        for (int i = 0; i < Clone.weight.length; i++) {
+        weight[i] = Clone.weight[i];
+        }
+        year = new float[Clone.year.length];
+        for (int i = 0; i < Clone.year.length; i++) {
+        year[i] = Clone.year[i];
+        }
+        price = new double[Clone.price.length];
+        for (int i = 0; i < Clone.price.length; i++) {
+        price[i] = Clone.price[i];
+        }
+        ammountKomp++;
+    }
+    
 
     public void println(String print){
         System.out.println(print);
     }
+
+      public void printKomp(){
+        println("Person's name is " + namePerson);
+        println("There are these many components in Komp " + components);
+        System.out.print("parts used are:");
+        for(int i=0;i<partModel.length;i++){
+            System.out.print(" "+partModel[i]);
+        }
+        println("");
+       System.out.print("weights of components are:");
+        for(int i=0;i<weight.length;i++){
+           System.out.print(" "+weight[i]);
+        }
+        println("");
+        System.out.print("years are: ");
+        for(int i=0;i<year.length;i++){
+           System.out.print(" "+year[i]);
+        }
+        println("");
+        System.out.print("prices are: ");
+        for(int i=0;i<price.length;i++){
+            System.out.print(" "+price[i]);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Komp Pervii=new Komp();
+        Pervii.printKomp();
     }
 }
